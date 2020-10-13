0fb0aac2d2834ddbea7e8080b49d5927a3359158
shubhi parolia <shubhiparolia55@gmail.com>
2020-10-13 17:28:09 +0200

    PV_studies

--- BcToJpsiTrkAnalyzer/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/BcAnalysis/BcToJpsiTrkAnalyzer/',1)[0])+'/cfipython/slc6_amd64_gcc700/BcAnalysis/BcToJpsiTrkAnalyzer')
